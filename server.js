940c52c10beaadc6bcaa799894e806d7419fdb2c
Claude Jordan <claudejordan@gmail.com>
2024-03-21 11:55:10 +0100

    updated environment variables

--- server.js
+++ server.js
@@ -1,99 +1,156 @@
 const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
+const knex = require("knex");
 
 const app = express();
+
+const db = knex({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false},
+    host: process.env.DATABSE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
+  }
+});
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = process.env.CLARIFAI_PAT;
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'kriskris';
+  const APP_ID = 'Smartbrain';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                      // "base64": IMAGE_BYTES_STRING
+                  }
+              }
+          }
+      ]
+  });
+
+  return {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+}
 
 app.use(express.json())
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "John",
-      email: "john@gmail.com",
-      password: "cookies",
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: "124",
-      name: "Sally",
-      email: "sally@gmail.com",
-      password: "bananas",
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-}
-
 app.get("/", (req, res) => {
-  res.send(database.users);
+  res.json("success");
 })
 
+const handleApiCall = (req, res) => {
+  fetch("https://api.clarifai.com/v2/models/" + "face-detection"+ "/outputs", returnClarifaiRequestOptions(req.body.input))
+  .then(response => response.text())
+  .then(result => res.json(result))
+  .catch(err => res.status(400).json("bad clarifai call"));
+}
+
+app.post("/imageurl", (req, res) => {
+  handleApiCall(req, res);
+})
 
 app.post("/signin", (req, res) => {
-  if (req.body.email === database.users[0].email
-    && req.body.password === database.users[0].password) {
-    res.json(database.users[0])
-  } else {
-    res.status(400).json("error logging in")
-  }
+  const {email, name, password } = req.body;
+  if (!email || !password) return res.status(400).json("incorrect form submission");
+
+  db.select("email", "hash").from("login")
+    .where("email", "=", email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash)
+      if (isValid) {
+        return db.select("*").from("users")
+          .where("email", "=", email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to get user"));
+      } else {
+        return res.status(400).json("wrong credentials");
+      }
+    }).catch(err => res.status(400).json("wrong credentials"));
 })
 
 app.post("/register", (req, res) => {
   const {email, name, password } = req.body;
-  bcrypt.hash(password, null, null, (err, hash) => {
-    console.log(hash);
+  if (!email || !name || !password) return res.status(400).json("incorrect form submission");
+
+  const hash = bcrypt.hashSync(password);
+
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into("login")
+    .returning("email")
+    .then(loginEmail => {
+      return trx("users")
+        .returning("*")
+        .insert({
+          email: loginEmail[0].email,
+          name: name,
+          joined: new Date()
+        }).then(user => {
+          res.json(user)
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
   })
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length-1])
+  .catch(err => res.status(400).json("Unable to register"))
 })
 
 app.get("/profile/:id", (req, res) => {
   const {id} = req.params;
   let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user)
+  db.select("*").from("users").where({id: id}).then(user => {
+    if (user.length) {
+      res.json(user[0])
+    } else {
+      res.status(400).json("Not found");
     }
   })
-  if (!found) res.status(400).json("not found")
+  .catch(err => res.status(400).json("Not found"));
 })
 
 app.put("/image", (req, res) => {
   const {id} = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries)
-    }
+  db("users").where("id", "=", id)
+  .increment("entries", 1)
+  .returning("entries")
+  .then(entries => {
+    res.json(entries[0])
   })
-  if (!found) res.status(400).json("not found")
+  .catch(err => res.status(400).json("Unable to get count"))
 })
 
-app.listen(3001, () => {
-  console.log("app is running on port 3001")
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`)
 });
-
-
-/*
-
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-
-*/
