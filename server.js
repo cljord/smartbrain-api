f6a2b3b0b8428508e843d6648215a6d40469a499
Claude Jordan <claudejordan@gmail.com>
2024-03-21 12:14:43 +0100

    console.log added

--- server.js
+++ server.js
@@ -99,6 +99,8 @@ app.post("/register", (req, res) => {
   const {email, name, password } = req.body;
   if (!email || !name || !password) return res.status(400).json("incorrect form submission");
 
+  console.log("hit register, req.body: ", req.body);
+
   const hash = bcrypt.hashSync(password);
 
   db.transaction(trx => {
